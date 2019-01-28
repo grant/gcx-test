38b8d16064ef17159e7f55cdb37b2aebb6e8b976
Grant Timmerman <timmerman+devrel@google.com>
2019-01-28 11:57:13 -0800

    Add multi-deploy command

--- functions/test1/index.js
+++ functions/test1/index.js
@@ -1,4 +1,8 @@
 const mymessage = require('./otherfile');
+
+// Test npm
+const PI = require("pi");
+
 /**
  * Responds to any HTTP request.
  *
@@ -7,5 +11,26 @@ const mymessage = require('./otherfile');
  */
 exports.grant1 = (req, res) => {
   let message = req.query.message || req.body.message || 'ok grant1!';
+  res.status(200).send(message + mymessage);
+};
+
+exports.grant2 = (req, res) => {
+  let message = req.query.message || req.body.message || 'nice grant2!';
+  res.status(200).send(message + mymessage);
+};
+
+exports.grant3 = (req, res) => {
+  let message = req.query.message || req.body.message || 'nice grant3!';
+  res.status(200).send(message + mymessage);
+};
+
+exports.grant4 = (req, res) => {
+  let message = req.query.message || req.body.message || 'nice grant4!';
+  message += PI(23);
+  res.status(200).send(message + mymessage);
+};
+
+exports.pi = (req, res) => {
+  let message = PI(23);
   res.status(200).send(message + mymessage);
 };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const gcx = require('gcx');
 
+const {readdirSync, lstatSync} = require('fs');
 const TARGET_DIR = './functions/test1';
 const CREDS_FILE_LOCATION = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const CREDS_FILE = require(CREDS_FILE_LOCATION);
@@ -9,14 +10,14 @@ console.log(`Using creds: ${CREDS_FILE_LOCATION}`);
  * Deploys a cloud function.
  * @param {string} name The name of the function in the TARGET_DIR.
  */
-const deploy = async (name) => {
+const deploy = async (name, targetDir) => {
   console.log(`Deploying ${name}...`);
   try {
     const dep = await gcx.deploy({
       name,
       region: 'us-central1',
       runtime: 'nodejs8',
-      targetDir: TARGET_DIR, // important!
+      targetDir, // important!
       description: 'test desc',
       retry: true,
       // maxInstances: 10, Alpha feature that needs whitelisting
@@ -39,7 +40,44 @@ const listFunctions = (dir) => {
   return fnNames;
 };
 
-const fns = listFunctions(TARGET_DIR);
-fns.map((fn) => {
-  deploy(fn);
-});
+/**
+ * Gets a list of directories within the functions directory.
+ */
+const getFunctionDirectories = () => {
+  const BASE_DIR = './functions';
+  const directories = [];
+  readdirSync(BASE_DIR).map((subdir) => {
+    const dir = `${BASE_DIR}/${subdir}`;
+    if (lstatSync(dir).isDirectory()) {
+      directories.push(dir);
+    }
+  });
+  return directories;
+};
+
+/**
+ * Tests the fn directories.
+ */
+const testFnDirs = () => {
+  const fndirs = getFunctionDirectories();
+  fndirs.map(fndir => {
+    const fns = listFunctions(fndir);
+    console.log(`- ${fndir}: [${fns}]`);
+  });
+}
+
+/**
+ * Deploys all functions in all directories.
+ */
+const deployAll = () => {
+  const fndirs = getFunctionDirectories();
+  fndirs.map(fndir => {
+    const fns = listFunctions(fndir);
+    fns.map((fn) => {
+      deploy(fn, TARGET_DIR);
+    });
+  });
+}
+
+// testFnDirs();
+deployAll();
